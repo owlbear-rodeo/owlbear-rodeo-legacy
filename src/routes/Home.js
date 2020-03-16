9ef3b8b213b38c644d8fe1f6f5360afbae3d52d1
nicola <nthouliss@gmail.com>
2020-03-16 21:50:40 +1100

    Add style to Homepage
    co-authored-by: mitchemmc <mitchemmc@gmail.com>

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { A } from "hookrouter";
+import { navigate } from "hookrouter";
+import { Container, Flex, Button } from "theme-ui";
 
 function Home() {
-    return <div><A href="/join">JOIN GAME</A>HOME!</div>
+  return (
+    <Container p={4} sx={{ maxWidth: "300px" }}>
+      <Flex sx={{ flexDirection: "column" }}>
+        <Button m={2} onClick={() => navigate("/game")}>
+          Start Game
+        </Button>
+        <Button m={2} onClick={() => navigate("/join")}>
+          Join Game
+        </Button>
+      </Flex>
+    </Container>
+  );
 }
 
-export default Home; 
+export default Home;
