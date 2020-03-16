c1e72766c5f5e1d5dad39a67a05b9612bda81234
nicola <nthouliss@gmail.com>
2020-03-16 21:51:32 +1100

    Format Game.js and Join.js files
    co-authored-by: mitchemmc <mitchemmc@gmail.com>

--- src/routes/Game.js
+++ src/routes/Game.js
@@ -2,7 +2,11 @@ import React from "react";
 import { A } from "hookrouter";
 
 function Game() {
-    return <div><A href="/">GO TO HOME</A>GAME!</div>
+  return (
+    <div>
+      <A href="/">GO TO HOME</A>GAME!
+    </div>
+  );
 }
 
 export default Game;
--- src/routes/Join.js
+++ src/routes/Join.js
@@ -2,7 +2,11 @@ import React from "react";
 import { A } from "hookrouter";
 
 function Join() {
-    return <div><A href="/game">GO TO GAME</A>JOIN!</div>
+  return (
+    <div>
+      <A href="/game">GO TO GAME</A>JOIN!
+    </div>
+  );
 }
 
 export default Join;
