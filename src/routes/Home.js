7d046c0a1a3794b0950ef3f757ae7bae080e36e7
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-04-14 09:53:26 +1000

    Bump version

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -46,7 +46,7 @@ function Home() {
           Join Game
         </Button>
         <Text variant="caption" as="p" sx={{ textAlign: "center" }}>
-          Alpha v0.9.0
+          Alpha v0.9.1
         </Text>
         <JoinModal
           isOpen={isJoinModalOpen}
