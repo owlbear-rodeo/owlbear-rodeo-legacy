506a84b67e33edfbd73d07278a0108651dba572d
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-04-14 16:13:49 +1000

    Fix version

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -49,7 +49,7 @@ function Home() {
           Join Game
         </Button>
         <Text variant="caption" as="p" sx={{ textAlign: "center" }}>
-          Alpha v0.10.2
+          Alpha v0.10.0
         </Text>
         <JoinModal
           isOpen={isJoinModalOpen}
