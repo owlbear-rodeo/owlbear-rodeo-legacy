d662f5e10704bf089091772b776eea270142cbdd
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-04-15 12:50:21 +1000

    Fix error if a modal is shown on page load

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 Modal.setAppElement("#root");
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
