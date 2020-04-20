5fa143293c8188303ba83db1859851b100cd29ff
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-04-20 15:20:09 +1000

    Fix grid off icon name

--- src/icons/GridOffIcon.js
+++ src/icons/GridOffIcon.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function GridOnIcon() {
+function GridOffIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -15,4 +15,4 @@ function GridOnIcon() {
   );
 }
 
-export default GridOnIcon;
+export default GridOffIcon;
