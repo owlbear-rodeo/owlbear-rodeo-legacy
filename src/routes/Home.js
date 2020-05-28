fb63af1ddd559c3456fc29f157d3bf68e462f2aa
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-05-29 08:43:54 +1000

    Bump version

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -51,7 +51,7 @@ function Home() {
           Join Game
         </Button>
         <Text variant="caption" as="p" sx={{ textAlign: "center" }}>
-          Beta v1.2.1
+          Beta v1.3.0
         </Text>
         <Button
           m={2}
