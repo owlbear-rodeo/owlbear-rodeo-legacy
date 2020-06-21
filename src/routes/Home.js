ccc0c2b89deba97001ce2bacdb19d6f19eed2e99
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-06-21 16:58:41 +1000

    Updated version to 1.4.0 preview

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -51,7 +51,7 @@ function Home() {
           Join Game
         </Button>
         <Text variant="caption" as="p" sx={{ textAlign: "center" }}>
-          Beta v1.3.3
+          Beta v1.4.0 (preview)
         </Text>
         <Button
           m={2}
