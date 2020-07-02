2c60cff0c765358743a065a52baa8bbcff914e07
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-07-02 17:00:50 +1000

    Change markdown note color

--- src/theme.js
+++ src/theme.js
@@ -270,8 +270,8 @@ export default {
       borderLeftColor: "#ff939b",
     },
     note: {
-      backgroundColor: "#cd9937",
-      borderLeftColor: "#ffe493",
+      backgroundColor: "#4e5d68",
+      borderLeftColor: "#6889a1",
     },
   },
 };
