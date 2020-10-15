396504fd85a3b3747ed33d1054b9f0b27fef9d7f
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-10-15 15:54:10 +1100

    Updated grid size model to be static

--- src/ml/gridSize/GridSizeModel.js
+++ src/ml/gridSize/GridSizeModel.js
@@ -6,20 +6,21 @@ import config from "./model.json";
 import weights from "./group1-shard1of1.bin";
 
 class GridSizeModel extends Model {
-  model;
+  // Store model as static to prevent extra network requests
+  static model;
   constructor() {
     super(config, { "group1-shard1of1.bin": weights });
   }
 
   async predict(imageData) {
-    if (!this.model) {
-      this.model = await tf.loadLayersModel(this);
+    if (!GridSizeModel.model) {
+      GridSizeModel.model = await tf.loadLayersModel(this);
     }
     const prediction = tf.tidy(() => {
       const image = tf.browser.fromPixels(imageData, 1).toFloat();
       const normalized = image.div(tf.scalar(255.0));
       const batched = tf.expandDims(normalized);
-      return this.model.predict(batched);
+      return GridSizeModel.model.predict(batched);
     });
     const data = await prediction.data();
     return data[0];
