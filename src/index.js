65cb346fa942087ebc729a0997de3b965a019ed6
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-10-27 08:48:29 +1100

    Removed sentry tracing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import Modal from "react-modal";
 
@@ -16,11 +15,6 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn:
       "https://bc1e2edfe7ca453f8e7357a48693979e@o467475.ingest.sentry.io/5493956",
-    integrations: [new Integrations.BrowserTracing()],
-
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
   });
 }
 
