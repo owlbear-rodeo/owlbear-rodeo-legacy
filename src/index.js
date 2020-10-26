28958d0769787c3679db997b53aba8faa3de5de9
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-10-27 08:02:41 +1100

    Added sentry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import Modal from "react-modal";
 
@@ -9,6 +11,18 @@ import "simplebar/dist/simplebar.min.css";
 import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn:
+      "https://bc1e2edfe7ca453f8e7357a48693979e@o467475.ingest.sentry.io/5493956",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
 
 Modal.setAppElement("#root");
 
