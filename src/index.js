15750c21439f22a20d0a8e50358f3b2d1f93a5b7
Mitchell McCaffrey <mitchemmc@gmail.com>
2020-11-28 08:51:56 +1100

    Added ignore to quota error

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ if (process.env.NODE_ENV === "production") {
       "https://bc1e2edfe7ca453f8e7357a48693979e@o467475.ingest.sentry.io/5493956",
     release: "owlbear-rodeo@" + process.env.REACT_APP_VERSION,
     // Ignore resize error as it is triggered by going fullscreen on slower computers
-    ignoreErrors: ["ResizeObserver loop limit exceeded"],
+    // Ignore quota error
+    ignoreErrors: ["ResizeObserver loop limit exceeded", "QuotaExceededError"],
   });
 }
 
