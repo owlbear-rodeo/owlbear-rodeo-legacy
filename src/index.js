4727ef4a0beabb237df8cbeca645368776a32134
Nicola Thouliss <nthouliss@gmail.com>
2021-02-25 15:46:03 +1100

    Added recommend errors and urls to ignore

--- src/index.js
+++ src/index.js
@@ -30,6 +30,43 @@ if (process.env.REACT_APP_LOGGING === "true") {
       "Extension context invalidated",
       "InvalidStateError",
       new RegExp("[a-zA-Z]*Failed to execute 'transaction' on 'IDBDatabase': The database connection is closing*"),
+      // Random plugins/extensions
+      "top.GLOBALS",
+      // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+      "originalCreateNotification",
+      "canvas.contentDocument",
+      "MyApp_RemoveAllHighlights",
+      "http://tt.epicplay.com",
+      "Can't find variable: ZiteReader",
+      "jigsaw is not defined",
+      "ComboSearch is not defined",
+      "http://loading.retry.widdit.com/",
+      "atomicFindClose",
+      // Facebook borked
+      "fb_xd_fragment",
+      // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+      // reduce this. (thanks @acdha)
+      // See http://stackoverflow.com/questions/4113268
+      "bmi_SafeAddOnload",
+      "EBCallBackMessageReceived",
+      // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+      "conduitPage",
+    ],
+    denyUrls: [
+      // Facebook flakiness
+      /graph\.facebook\.com/i,
+      // Facebook blocked
+      /connect\.facebook\.net\/en_US\/all\.js/i,
+      // Woopra flakiness
+      /eatdifferent\.com\.woopra-ns\.com/i,
+      /static\.woopra\.com\/js\/woopra\.js/i,
+      // Chrome extensions
+      /extensions\//i,
+      /^chrome:\/\//i,
+      // Other plugins
+      /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+      /webappstoolbarba\.texthelp\.com\//i,
+      /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
     ],
   });
 }
