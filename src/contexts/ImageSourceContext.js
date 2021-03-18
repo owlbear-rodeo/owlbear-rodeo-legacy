44aafbe2f312fc88db7f9266fac448e177dd6e92
Mitchell McCaffrey <mitchemmc@gmail.com>
2021-03-18 11:25:29 +1100

    Remove unused import

--- src/contexts/ImageSourceContext.js
+++ src/contexts/ImageSourceContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect, useRef } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import { omit } from "../helpers/shared";
 
