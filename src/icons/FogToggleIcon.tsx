6f28db078968ea222116964212c11fcbfdf662fc
Nicola Thouliss <nthouliss@gmail.com>
2021-05-27 16:10:54 +1000

    Converted icons to typescript

---
+++ src/icons/FogToggleIcon.tsx
@@ -0,0 +1,16 @@
+function FogToggleIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+      fill="currentcolor"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 4a7.49 7.49 0 017.35 6.04c2.6.18 4.65 2.32 4.65 4.96 0 2.76-2.24 5-5 5H6c-3.31 0-6-2.69-6-6 0-3.09 2.34-5.64 5.35-5.96A7.496 7.496 0 0112 4zm0 2a5.493 5.493 0 00-4.759 2.75l-.117.214-.496.951-1.067.114A3.994 3.994 0 002 14a4.005 4.005 0 003.8 3.995L6 18h6.042v-.103L12 6z" />
+    </svg>
+  );
+}
+
+export default FogToggleIcon;
