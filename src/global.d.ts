349cad53a2a9e520c785c3d8fad33b3685e69f68
Nicola Thouliss <nthouliss@gmail.com>
2021-05-28 12:37:33 +1000

    Converted assets index.js to typescript

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,7 @@
 declare module 'pepjs';
 declare module 'socket.io-msgpack-parser';
 declare module 'fake-indexeddb';
-declare module 'fake-indexeddb/lib/FDBKeyRange'
+declare module 'fake-indexeddb/lib/FDBKeyRange';
+declare module '*.glb';
+declare module '*.png';
+declare module '*.mp4';
