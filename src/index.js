675dd2b37cc89cb25bd014c8e6b3094f6fb28157
Mitchell McCaffrey <mitchemmc@gmail.com>
2021-06-20 15:07:06 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
-import Dedupe from "@sentry/integrations";
+import { Dedupe } from "@sentry/integrations";
 import App from "./App";
 import Modal from "react-modal";
 
